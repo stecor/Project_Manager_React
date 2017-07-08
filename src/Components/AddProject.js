e4e5b126a268fde461fca70cfd49ef7917a95a70
Stefano Corra <stefano.corra@hotmail.com>
2017-07-08 13:35:39 -0400

    fourth commit

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import uuid from 'uuid';
 
 
 class AddProject extends Component {
@@ -20,6 +20,7 @@ class AddProject extends Component {
       alert('Title is required');
     }else{
       this.setState({newProject:{
+        id: uuid.v4(),
         title: this.refs.title.value,
         category: this.refs.category.value
       }}, function(){
@@ -48,11 +49,18 @@ class AddProject extends Component {
             {categoryOptions}
             </select>
           </div>
+          <br/>
           <input type="submit" value="Submit"/>
+          <br/>
         </form>
       </div>
     );
   }
+}
+
+AddProject.propTypes = {
+  categories:React.PropTypes.array,
+  addProject:React.PropTypes.func
 }
 
 export default AddProject;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,22 +4,32 @@ import ProjectItem from './ProjectItem';
 
 class Projects extends Component {
 
+  deleteProject(id){
+    this.props.onDelete(id);
+  }
+
   render() {
     let projectItems;
     if(this.props.projects){
       projectItems = this.props.projects.map(project => {
         //console.log(project);
         return(
-          <ProjectItem key={project.title} project={project}/>
+          <ProjectItem onDelete={this.deleteProject.bind(this)} key={project.title} project={project}/>
         )
       });
     }
     return (
       <div className="Projects">
+        <h3>Latest Projects</h3>
         {projectItems}
       </div>
     );
   }
+}
+
+Projects.propTypes = {
+  projects:React.PropTypes.array,
+  onDelete:React.PropTypes.func
 }
 
 export default Projects;
